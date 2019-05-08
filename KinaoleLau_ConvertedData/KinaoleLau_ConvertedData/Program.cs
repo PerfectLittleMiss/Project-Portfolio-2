b28e67d1aaf4de05c966143be14c17f39517c3cf
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-07 14:50:00 -1000

    Added the necessary commented out information and created the print menu function for listing top menu options.

--- KinaoleLau_ConvertedData/KinaoleLau_ConvertedData/Program.cs
+++ KinaoleLau_ConvertedData/KinaoleLau_ConvertedData/Program.cs
@@ -10,6 +10,24 @@ namespace KinaoleLau_ConvertedData
     {
         static void Main(string[] args)
         {
+
+            //Database Location
+            //string cs = @"server= 127.0.0.1;userid=root;password=root;database=SampleRestaurantDatabase;port=8889";
+            //Output Location
+            //string _directory = @"../../output/";
+
+
+        }
+
+        public void PrintMenu()
+        {
+            Console.WriteLine("Hello Admin, What Would You Like To Do Today?");
+            // List top menu options only
+            Console.WriteLine("\n[1] Convert the restaurant reviews database from SQL to JSON");
+            Console.WriteLine("[2] Showcase our 5 star rating system");
+            Console.WriteLine("[3] Showcase our animated bar graph review system");
+            Console.WriteLine("[4] Play a card game");
+            Console.WriteLine("[5] Exit");
         }
     }
 }
