2de0339be7393b03442fac492dd67d0d802d8bca
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-07 17:23:11 -1000

    Added similar functions to get and save the data from the other two tables in the db.

--- KinaoleLau_ConvertedData/KinaoleLau_ConvertedData/DatabaseFunctions.cs
+++ KinaoleLau_ConvertedData/KinaoleLau_ConvertedData/DatabaseFunctions.cs
@@ -89,5 +89,124 @@ namespace KinaoleLau_ConvertedData
 
             return DBInfo;
         }
+
+        public Dictionary<int, Dictionary<string, string>> GetRestaurantReviewersDBInfo()
+        {
+            //Empty dictionary to hold the row number as the key and the column name and value for that row as the value
+            // Values will be converted back later
+            Dictionary<int, Dictionary<string, string>> DBInfo = new Dictionary<int, Dictionary<string, string>>();
+
+            // Use try catch to connect to database, get data into datatable and save data to dictionary
+            try
+            {
+                MySqlConnection conn = null;
+                conn.Open();
+
+                string stm = "Select * from RestaurantReviewers" +
+                    "";
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(stm, conn);
+                DataTable dbInfo = new DataTable();
+
+                adapter.SelectCommand.CommandType = CommandType.Text;
+                adapter.Fill(dbInfo);
+
+                DataColumnCollection dbCols = dbInfo.Columns;
+
+                //Index to keep track of row number
+                int index = 0;
+
+                foreach (DataRow row in dbInfo.Rows)
+                {
+                    // Empty dictionary to contain the column names and values for each row
+                    Dictionary<string, string> rowValues = new Dictionary<string, string>();
+
+                    foreach (DataColumn col in dbCols)
+                    {
+                        //Get the column name and value for each column in the row
+                        string key = col.ColumnName;
+                        string value = row[col].ToString();
+
+                        // Add the values to the dictionary containing the row data
+                        rowValues.Add(key, value);
+                    }
+
+                    // Add the row number, the index, and the rowValues dictionary to the dictionary containing the entire databases's info
+                    DBInfo.Add(index, rowValues);
+
+                    index++;
+                }
+
+                conn.Close();
+
+            }
+            catch (MySqlException e)
+            {
+                string msg = e.ToString();
+                Console.WriteLine(msg);
+            }
+
+            return DBInfo;
+        }
+
+        public Dictionary<int, Dictionary<string, string>> GetRestaurantReviewsDBInfo()
+        {
+            //Empty dictionary to hold the row number as the key and the column name and value for that row as the value
+            // Values will be converted back later
+            Dictionary<int, Dictionary<string, string>> DBInfo = new Dictionary<int, Dictionary<string, string>>();
+
+            // Use try catch to connect to database, get data into datatable and save data to dictionary
+            try
+            {
+                MySqlConnection conn = null;
+                conn.Open();
+
+                string stm = "Select * from RestaurantReviews";
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(stm, conn);
+                DataTable dbInfo = new DataTable();
+
+                adapter.SelectCommand.CommandType = CommandType.Text;
+                adapter.Fill(dbInfo);
+
+                DataColumnCollection dbCols = dbInfo.Columns;
+
+                //Index to keep track of row number
+                int index = 0;
+
+                foreach (DataRow row in dbInfo.Rows)
+                {
+                    // Empty dictionary to contain the column names and values for each row
+                    Dictionary<string, string> rowValues = new Dictionary<string, string>();
+
+                    foreach (DataColumn col in dbCols)
+                    {
+                        //Get the column name and value for each column in the row
+                        string key = col.ColumnName;
+                        string value = row[col].ToString();
+
+                        // Add the values to the dictionary containing the row data
+                        rowValues.Add(key, value);
+                    }
+
+                    // Add the row number, the index, and the rowValues dictionary to the dictionary containing the entire databases's info
+                    DBInfo.Add(index, rowValues);
+
+                    index++;
+                }
+
+                conn.Close();
+
+            }
+            catch (MySqlException e)
+            {
+                string msg = e.ToString();
+                Console.WriteLine(msg);
+            }
+
+            return DBInfo;
+        }
+
+
     }
 }
