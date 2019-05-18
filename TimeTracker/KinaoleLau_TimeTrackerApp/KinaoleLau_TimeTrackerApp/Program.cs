b3fb40a808a98391d400ef98691f1ec3a7a8eccf
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-18 13:32:51 -1000

    Created time tracker app program and began creating get categories function.

---
+++ TimeTracker/KinaoleLau_TimeTrackerApp/KinaoleLau_TimeTrackerApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinaoleLau_TimeTrackerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void PrintCommands()
+        {
+            // Clear the console
+            Console.Clear();
+
+            Console.WriteLine("[1] Enter Activity");
+            Console.WriteLine("[2] View Tracked Data");
+            Console.WriteLine("[3] Run Calculations");
+            Console.WriteLine("[4] Exit");
+        }
+    }
+}
