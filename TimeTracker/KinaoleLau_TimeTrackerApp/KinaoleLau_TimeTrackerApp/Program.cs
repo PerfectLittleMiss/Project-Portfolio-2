3a719f4239c8c20d01ed2a3823444bab28e24f82
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-18 14:05:18 -1000

    Added empty menu with all main options to main.

--- TimeTracker/KinaoleLau_TimeTrackerApp/KinaoleLau_TimeTrackerApp/Program.cs
+++ TimeTracker/KinaoleLau_TimeTrackerApp/KinaoleLau_TimeTrackerApp/Program.cs
@@ -10,6 +10,48 @@ namespace KinaoleLau_TimeTrackerApp
     {
         static void Main(string[] args)
         {
+            // bool to determine if program is running
+            bool running = true;
+
+            while(running)
+            {
+                PrintCommands();
+                string choice = Validation.GetString("Enter your choice: ").ToLower();
+
+                switch(choice)
+                {
+                    case "1":
+                    case "enter activity":
+
+                        break;
+
+                    case "2":
+                    case "view tracked data":
+                    case "view data":
+
+                        break;
+
+                    case "3":
+                    case "run calculations":
+
+                        break;
+
+                    case "4":
+                    case "exit":
+                    case "quit":
+                        running = false;
+                        Console.WriteLine("You are now exiting the program.");
+                        Console.WriteLine("Goodbye!");
+                        Console.WriteLine("Press any key to exit...");
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid command. Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
         }
 
         public static void PrintCommands()
