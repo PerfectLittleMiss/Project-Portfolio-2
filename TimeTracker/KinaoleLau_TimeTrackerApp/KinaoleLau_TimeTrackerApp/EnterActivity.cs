0b5e22b988d77b2fb9e2bd1e7494d7eb4042e6f3
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-18 14:11:30 -1000

    Created enter activity class and added print commands functions.

---
+++ TimeTracker/KinaoleLau_TimeTrackerApp/KinaoleLau_TimeTrackerApp/EnterActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinaoleLau_TimeTrackerApp
+{
+    class EnterActivity
+    {
+        public static void ActivityMenu()
+        {
+
+        }
+
+        private static void PrintCommands()
+        {
+            // Clear console
+            Console.Clear();
+
+            Console.WriteLine("Pick a category of activity:");
+
+            List<string> categories = DatabaseFunctions.GetCategories();
+
+            // Create index variable to keep track of category number
+            int index = 1;
+
+            // Loop through categories and print them as options to the screen
+            foreach(string category in categories)
+            {
+                Console.WriteLine($"- [{0}] {1}", index, category);
+            }
+        }
+    }
+}
