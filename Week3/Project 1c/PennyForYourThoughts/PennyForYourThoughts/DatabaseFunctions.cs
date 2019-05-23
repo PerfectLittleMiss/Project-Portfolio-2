04a34bbb19ae0a7c8b888d3ff01e605cbd69368f
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-22 15:42:53 -1000

    Finished coding the login functionality.

--- Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/DatabaseFunctions.cs
+++ Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/DatabaseFunctions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -23,6 +24,167 @@ namespace PennyForYourThoughts
             string conString = $"Server={ip};userid=root;password=root;database=KinaoleLau_Database;port=8889";
 
             return conString;
+        }
+
+        public static string CheckUsername(string username)
+        {
+            // Returns user password to check in login class if username exists, else returns password variable with value of false
+            string password = null;
+
+            // Use try catch to connect to database, get data into datatable and save data to dictionary
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(GetConnString());
+                conn.Open();
+
+                MySqlDataReader rdr = null;
+
+                string stm = "Select * from users where username = @username";
+
+                MySqlCommand cmd = new MySqlCommand(stm, conn);
+
+                cmd.Parameters.AddWithValue("@username", username);
+
+                rdr = cmd.ExecuteReader();
+
+
+                while (rdr.Read())
+                {
+                    // Save password to password variable
+                    password = rdr["password"].ToString();
+                }
+
+                //if no username existed in the database the password variable will remain null so set it to false
+                if(string.IsNullOrWhiteSpace(password))
+                {
+                    password = "false";
+                }
+                
+                //else there was a user with the username already in the database so do NOT change the password value
+
+                conn.Close();
+
+            }
+            catch (MySqlException e)
+            {
+                string msg = e.ToString();
+                Console.WriteLine(msg);
+            }
+            // return the variable password
+            return password;
+        }
+
+        public static string CheckUsernameAndPassword(string username, string password)
+        {
+            // Returns user password to check in login class if username exists, else returns password variable with value of false
+            string name = null;
+
+            // Use try catch to connect to database, get data into datatable and save data to dictionary
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(GetConnString());
+                conn.Open();
+
+                MySqlDataReader rdr = null;
+
+                string stm = "Select * from users where username = @username and password = @password";
+
+                MySqlCommand cmd = new MySqlCommand(stm, conn);
+
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                rdr = cmd.ExecuteReader();
+
+
+                while (rdr.Read())
+                {
+                    // Save name to name variable
+                    name = rdr["name"].ToString();
+                }
+
+                //if the username and password in the database don't match the variable will remain null so set it to false
+                if (string.IsNullOrWhiteSpace(password))
+                {
+                    name = "false";
+                }
+
+                //else the username and password do match so leave the name variable alone
+
+                conn.Close();
+
+            }
+            catch (MySqlException e)
+            {
+                string msg = e.ToString();
+                Console.WriteLine(msg);
+            }
+            // return the variable name
+            return name;
+        }
+
+        public static void AddUser(string username, string password, string name)
+        {
+            // Use try catch to connect to database, get data into datatable and save data to dictionary
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(GetConnString());
+                conn.Open();
+
+                MySqlDataReader rdr = null;
+
+                // Insert new user into database
+                string stm = "Insert into users (name, username, password) values (@name, @username, @password)";
+
+                MySqlCommand cmd = new MySqlCommand(stm, conn);
+
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.AddWithValue("@name", name);
+
+                rdr = cmd.ExecuteReader();
+
+                // check that user was successfully inserted
+                stm = "Select * from users where username = @username and password = @password and name = @name";
+
+                cmd = new MySqlCommand(stm, conn);
+
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.AddWithValue("@name", name);
+
+                rdr = cmd.ExecuteReader();
+
+                string nameTest = null;
+
+                while (rdr.Read())
+                {
+                    // Save name to name variable
+                    nameTest = rdr["name"].ToString();
+                }
+
+                // if no name was returned then the insert failed
+                if (string.IsNullOrWhiteSpace(nameTest))
+                {
+                    Console.WriteLine("Insert statement failed.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("User created successfully!");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+
+                conn.Close();
+
+            }
+            catch (MySqlException e)
+            {
+                string msg = e.ToString();
+                Console.WriteLine(msg);
+            }
         }
     }
 }
--- Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/Login.cs
+++ Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/Login.cs
@@ -25,7 +25,7 @@ namespace PennyForYourThoughts
                 {
                     case "1":
                     case "login":
-
+                        LoginUser();
                         break;
 
                     case "2":
@@ -53,9 +53,70 @@ namespace PennyForYourThoughts
 
         private static void PrintCommands()
         {
+            Console.Clear();
+
             Console.WriteLine("[1] Login");
             Console.WriteLine("[2] Create a new account");
             Console.WriteLine("[3] Exit");
+        }
+
+        private static void CreateUser()
+        {
+            // bool to determine if user is still here
+            bool running = true;
+            while(running)
+            {
+                Console.WriteLine("You have chosen to create a new user.");
+                Console.WriteLine("If you wish to go back to the main menu type 0 and the return key.");
+                string name = Validation.GetString("Enter your name: ");
+
+                if(name == "0")
+                {
+                    running = false;
+                    Console.WriteLine("Returning to the main menu. Press any key to continue...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    string username = Validation.GetString("Enter a username (0 and return key if you want to go back to the main menu): ");
+
+                    if(username == "0")
+                    {
+                        running = false;
+                        Console.WriteLine("Returning to the main menu. Press any key to continue...");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        string checkUsername = DatabaseFunctions.CheckUsername(username);
+
+                        if(checkUsername == "false" || string.IsNullOrWhiteSpace(checkUsername))
+                        {
+                            // the username does not already exist so ask for a password
+                            string password = Validation.GetString("Enter a password (0 and return key to go back to the main menu): ");
+
+                            if(password == "0")
+                            {
+                                running = false;
+                                Console.WriteLine("Returning to the main menu. Press any key to continue...");
+                                Console.ReadKey();
+                            }
+                            else
+                            {
+                                // add user to the database then take user to the profile page
+                                DatabaseFunctions.AddUser(username, password, name);
+                            }
+                        }
+                        else
+                        {
+                            // the username already exists
+                            Console.WriteLine("That username already exists. To login go back to the main menu.");
+                            Console.WriteLine("Press any key to continue...");
+                            Console.ReadKey();
+                        }
+                    }
+                }
+            }
         }
 
         private static void LoginUser()
@@ -67,9 +128,9 @@ namespace PennyForYourThoughts
                 Console.WriteLine("To login we will need your username and password.");
                 Console.WriteLine("Enter 0 and the return key to go back");
                 Console.WriteLine("Otherwise please fill in your username and password");
-                string choice = Validation.GetString("Enter your username: ").ToLower();
+                string username = Validation.GetString("Enter your username: ").ToLower();
 
-                if(choice == "0")
+                if(username == "0")
                 {
                     running = false;
                     Console.WriteLine("Returning you to the welcome screen. Press any key to continue...");
@@ -77,7 +138,29 @@ namespace PennyForYourThoughts
                 }
                 else
                 {
+                    string password = DatabaseFunctions.CheckUsername(username);
 
+                    if(password == "false" || string.IsNullOrWhiteSpace(password))
+                    {
+                        Console.WriteLine("That username does not exist. If you'd like to create an account please go back to the main menu.");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        string name = DatabaseFunctions.CheckUsernameAndPassword(username, password);
+
+                        if(name == "false" || string.IsNullOrWhiteSpace(name))
+                        {
+                            Console.WriteLine("Invalid login. Please try again.");
+                            Console.WriteLine("Press any key to continue...");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            //Login was successful so take the user to the user profile page
+                        }
+                    }
                 }
             }
         }
