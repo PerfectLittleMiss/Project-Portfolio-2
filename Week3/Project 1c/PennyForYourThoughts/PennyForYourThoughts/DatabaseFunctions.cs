780d36a8b4fc716d13a24eb74ef4cb8dcf82fc0e
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-22 15:02:49 -1000

    Created the login menu and began creating the login user functionality.

--- Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/DatabaseFunctions.cs
+++ Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/DatabaseFunctions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace PennyForYourThoughts
 {
     class DatabaseFunctions
     {
+        private static string GetConnString()
+        {
+            //Empty until imported from fle
+            string ip = "";
+
+            using (StreamReader sr = new StreamReader("c:/VFW/connection.txt"))
+            {
+                // Sets ip value to line in file
+                ip = sr.ReadLine();
+            }
+            //Set conString with ip from file
+            string conString = $"Server={ip};userid=root;password=root;database=KinaoleLau_Database;port=8889";
+
+            return conString;
+        }
     }
 }
--- Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/Login.cs
+++ Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/Login.cs
@@ -8,5 +8,78 @@ namespace PennyForYourThoughts
 {
     class Login
     {
+        public static void LoginMenu()
+        {
+            Console.WriteLine("Welcome to Penny For Your Thoughts!");
+            Console.WriteLine("Please login or create an account to get started.");
+
+            // bool to determine if user is on this menu
+            bool running = true;
+
+            while(running)
+            {
+                PrintCommands();
+                string choice = Validation.GetString("Enter your choice: ").ToLower();
+
+                switch(choice)
+                {
+                    case "1":
+                    case "login":
+
+                        break;
+
+                    case "2":
+                    case "create a new account":
+
+                        break;
+
+                    case "3":
+                    case "exit":
+                    case "quit":
+                        running = false;
+                        Console.WriteLine("It's sad to see you go.");
+                        Console.WriteLine("Byee~");
+                        Console.WriteLine("Press any key to exit...");
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid command. Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        private static void PrintCommands()
+        {
+            Console.WriteLine("[1] Login");
+            Console.WriteLine("[2] Create a new account");
+            Console.WriteLine("[3] Exit");
+        }
+
+        private static void LoginUser()
+        {
+            // bool to determine if user is still here
+            bool running = true;
+            while(running)
+            {
+                Console.WriteLine("To login we will need your username and password.");
+                Console.WriteLine("Enter 0 and the return key to go back");
+                Console.WriteLine("Otherwise please fill in your username and password");
+                string choice = Validation.GetString("Enter your username: ").ToLower();
+
+                if(choice == "0")
+                {
+                    running = false;
+                    Console.WriteLine("Returning you to the welcome screen. Press any key to continue...");
+                    Console.ReadKey();
+                }
+                else
+                {
+
+                }
+            }
+        }
     }
 }
