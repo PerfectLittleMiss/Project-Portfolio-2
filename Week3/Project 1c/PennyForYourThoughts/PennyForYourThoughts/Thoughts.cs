f6529c19071f046b5ca53e7e719a0b6ba1b0cd05
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-22 16:54:36 -1000

    Created thoughts class and menu structure.

---
+++ Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/Thoughts.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PennyForYourThoughts
+{
+    class Thoughts
+    {
+        public static void Menu(int userId)
+        {
+            // bool to determine if user is on page
+            bool running = true;
+            while(running)
+            {
+                PrintCommands();
+                string choice = Validation.GetString("Enter your choice: ").ToLower();
+
+                switch(choice)
+                {
+                    case "1":
+                    case "search thoughts":
+
+                        break;
+                    case "2":
+                    case "select thought":
+
+                        break;
+                    case "3":
+                    case "create thought":
+
+                        break;
+                    case "4":
+                    case "view profile":
+                        running = false;
+                        Console.WriteLine("Returning to your profile. Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command. Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        private static void PrintCommands()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Here lies your thoughts:");
+            Console.WriteLine("[1] Search Thoughts");
+            Console.WriteLine("[2] Select Thought");
+            Console.WriteLine("[3] Create Thought");
+            Console.WriteLine("[4] View Profile");
+        }
+    }
+}
