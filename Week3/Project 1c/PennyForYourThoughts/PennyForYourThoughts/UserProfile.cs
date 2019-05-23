2f95ddcef2ae7ca3b7bd4e13be1ef970a8934c83
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-22 16:00:26 -1000

    Added menu and log out functionality to the user profile class.

--- Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/UserProfile.cs
+++ Week3/Project 1c/PennyForYourThoughts/PennyForYourThoughts/UserProfile.cs
@@ -15,7 +15,28 @@ namespace PennyForYourThoughts
 
             while(running)
             {
-                
+                PrintCommands(name, username);
+                string choice = Validation.GetString("Enter your choice: ").ToLower();
+
+                switch(choice)
+                {
+                    case "1":
+                    case "view/edit thoughts":
+
+                        break;
+
+                    case "2":
+                    case "log out":
+                        running = false;
+                        Console.WriteLine("Logging you out. Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid command. Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
             }
         }
 
@@ -28,7 +49,8 @@ namespace PennyForYourThoughts
             Console.WriteLine("Username: {0}", username);
             int count = DatabaseFunctions.GetThoughtCount(username);
             Console.WriteLine("Thought Count: {0}", count);
-            Console.WriteLine("\n[1] View Thoughts");
+            Console.WriteLine("\nWhat would you like to do now?");
+            Console.WriteLine("[1] View/Edit Thoughts");
             Console.WriteLine("[2] Log Out");
         }
     }
