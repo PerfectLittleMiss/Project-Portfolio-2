96187a33ff31aca726b9a7f46a9d6b8c93cc8601
PerfectLittleMiss <perfectlittlemiss1010@gmail.com>
2019-05-31 15:48:27 -1000

    Began creating card class.

---
+++ KinaoleLau_ConvertedData/KinaoleLau_ConvertedData/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinaoleLau_ConvertedData
+{
+    class Card
+    {
+        public string suit { get; set; }
+        public string name { get; set; }
+        public int points { get; set; }
+        public string color { get; set; }
+        public string background = "White";
+
+        public Card(string suit, string name, int points, string color)
+        {
+
+        }
+    }
+}
